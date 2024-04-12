c5b6a403345e4f47ae6754f51cc93db8a9cb8291
Thomas BRETON <thomas.breton@livurstay.com>
2024-04-12 16:10:48 +0200

    fix error

--- public/pwa/sw.js
+++
@@ -1,9 +0,0 @@
-//lors de l'installation
-self.addEventListener('install' ,evt =>{
-})
-
-
-//capture des events
-self.addEventListener('fetch' ,evt =>{
-    console.log('fetch evt sur url' ,evt.request.url);
-})
