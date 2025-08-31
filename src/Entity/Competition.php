2d2d6d71313bb61179a1164a2dd40163c5b9304a
Thomas BRETON <thomas.breton@livurstay.com>
2025-08-31 22:54:01 +0200

    fix default controller

--- src/Entity/Competition.php
+++ src/Entity/Competition.php
@@ -77,7 +77,7 @@ class Competition
 
     public function __toString()
     {
-        return $this->name." (".$this->season.")";
+        return $this->name." (".$this->seasonx.")";
     }
 
     /*****************************************************************************************************************
